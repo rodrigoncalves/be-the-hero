5d36643eebbf85ed9b25b6d0192127e167c031bc
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-24 13:20:56 -0300

    Create database

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.post('/users', (req, res) => {
+  const body = req.body
+  console.log(body)
+
+  return res.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Rodrigo Gonçalves'
+  })
+})
+
+module.exports = routes
