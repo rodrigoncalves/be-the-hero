75e7678c90dec7b27ca3c6686597a7ccbf312959
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-25 16:41:21 -0300

    New incident page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Logon from './pages/Logon'
 import Register from './pages/Register'
 import Profile from './pages/Profile'
+import NewIncident from './pages/NewIncident'
 
 export default () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ export default () => (
       <Route path="/" exact component={Logon}></Route>
       <Route path="/register" component={Register}></Route>
       <Route path="/profile" component={Profile}></Route>
+      <Route path="/incidents/new" component={NewIncident}></Route>
     </Switch>
   </BrowserRouter>
 )
