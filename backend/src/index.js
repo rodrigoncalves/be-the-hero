44cab8e1f01d6b8763e5375e980831b650cd23d3
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-26 00:41:36 -0300

    Session api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const cors = require('cors')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
-app.listen(3000)
+app.listen(3333)
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -7,11 +7,12 @@ import './styles.css'
 import logoImg from '../../assets/logo.svg'
 
 export default function Profile() {
+  const ongName = localStorage.getItem('ongName')
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link to="/incidents/new" className="button">
           Cadastrar novo caso
